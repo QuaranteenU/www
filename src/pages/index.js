bb4885eba8a2fb304717b3b561dac95bacaf0723
Warren-Partridge <maps@perdix.org>
2020-03-19 21:16:38 -0400

    Display number of signed up users

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,39 +8,74 @@ import FAQs from "../components/faqs"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Jumbotron fluid className="landing">
-      <Container>
-        <h1>Commencement 2020 is on!</h1>
-        <p>
-          Forced to leave your campus and your friends? Stuck in quarantine? Well, so are we but COVID-19 won't stop Quaranteen University. Come graduate with us in Minecraft, May 22 2020.
-        </p>
-        <Link to="/register">
-          <Button>
-            Add your name to the graduating class, lol
-          </Button>
-        </Link>
-      </Container>
-    </Jumbotron>
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countSignedUp: -1
+    };
+  }
 
-    <Container>
-      <h3>Walk the stage and receive your undergrad or grad diploma, virtually</h3>
-      <p>We're going to livestream it, because we really don't have anything better to be doing under quarantine</p>
+  componentDidMount() {
+    const url = "https://sheets.googleapis.com/v4/spreadsheets/1WANjh9qm28VFb5pF6Ba3rMdvG1r93DbUpg-HdTxjs6c/values/Sheet1!A1:A3?key=AIzaSyA-pLbYH5fK9S3b2nmnog6fc1XkSY-eG6M";
+    fetch(url, {
+      method: "GET",
+    }).then(res => res.json())
+      .then(
+        (result) => {
+          // console.log("good", result);
+          this.setState({
+            countSignedUp: result.values[0][0]
+          });
+        },
+        (error) => {
+          // console.log("bad", error);
+          this.setState({
+            countSignedUp: -1
+          });
+        }
+      )
+  }
 
 
-      <br/><br/><br/><br/>
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <Jumbotron fluid className="landing">
+          <Container>
+            <h1>Commencement 2020 is on!</h1>
+            <p>Num signed up:</p>
+            <p>{this.state.countSignedUp}</p>
+            <p>
+              Forced to leave your campus and your friends? Stuck in quarantine? Well, so are we but COVID-19 won't stop Quaranteen University. Come graduate with us in Minecraft, May 22 2020.
+            </p>
+            <Link to="/register">
+              <Button>
+                Add your name to the graduating class, lol
+              </Button>
+            </Link>
+          </Container>
+        </Jumbotron>
+
+        <Container>
+          <h3>Walk the stage and receive your undergrad or grad diploma, virtually</h3>
+          <p>We're going to livestream it, because we really don't have anything better to be doing under quarantine</p>
 
 
-      <h3>Another header</h3>
-      <p>Another sub header</p>
+          <br/><br/><br/><br/>
 
-      <br/><br/><br/><br/>
 
-      <FAQs />
-    </Container>
-  </Layout>
-)
+          <h3>Another header</h3>
+          <p>Another sub header</p>
+
+          <br/><br/><br/><br/>
+
+          <FAQs />
+        </Container>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage

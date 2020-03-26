52b71d41bfdebb64b077d49944f48a85bc33d72f
Rudhra Raveendran <rudytheninja@gmail.com>
2020-03-26 13:10:54 -0400

    New Release

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6f130af5398db0cc0be8.js"
+    "url": "webpack-runtime-1ab728c73491db3a5f2f.js"
   },
   {
-    "url": "commons-ad415bdd21b91fa150b3.js"
+    "url": "commons-ce2018dc95e4985f3271.js"
   },
   {
     "url": "app-6631b6bea2e24f5daf09.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d8a5ad1eecb89726f962e785a49020fc"
+    "revision": "94075f512c4e171404f23dc8f83be079"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0159bb149368cfb6a796.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "908a9929af00cbc3671ff7fa202f39e0"
+    "revision": "ef8d49651529472da01893776b7c9585"
   },
   {
     "url": "styles.2dee21cd1f2df8b9664d.css"
@@ -89,7 +89,7 @@ self.__precacheManifest = [
     "revision": "7ccb1677a3924eb7498e27ccb409e361"
   },
   {
-    "url": "component---src-pages-register-js-39ee83dde15849636742.js"
+    "url": "component---src-pages-register-js-f3909f1ae714868ab5d9.js"
   },
   {
     "url": "page-data/register/page-data.json",

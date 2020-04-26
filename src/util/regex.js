0d98d04b5b5413457dc1ba7b8174215586e4b58b
Rudhra Raveendran <rudytheninja@gmail.com>
2020-04-26 13:18:51 -0400

    Add edu.in

--- src/util/regex.js
+++ src/util/regex.js
@@ -1,4 +1,4 @@
 export const universityEmailPattern =
-  "[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+(?:edu|org|ac.uk|ca|edu.tr|ac.nz|edu.au)"; //eslint-disable-line
+  "[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+(?:edu|org|ac.uk|ca|edu.tr|ac.nz|edu.au|edu.in)"; //eslint-disable-line
 export const simpleEmailPattern =
   "[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+(?:[a-z0-9]{2,})"; //eslint-disable-line

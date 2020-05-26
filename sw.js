55eb58de337365f3287ab5cf1dbf5fafa7cc0da4
Rudhra Raveendran <rudytheninja@gmail.com>
2020-05-25 21:49:23 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,24 +27,24 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-86407499ba1cadc5a116.js"
+    "url": "webpack-runtime-df5334aa325f6c9cc4ae.js"
   },
   {
-    "url": "framework-3a6bacd9c2226b582de5.js"
+    "url": "framework-a272a84775ae56a7ce05.js"
   },
   {
-    "url": "app-2e801e2360f8b32ac8b0.js"
+    "url": "app-fe28ff561e652c8a47c5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1b70d72164de454b7da2e1b64ace3868"
+    "revision": "92b1eb4cf074d587becea4a4c31a3f97"
   },
   {
     "url": "google-fonts/s/librebaskerville/v7/kmKnZrc3Hgbbcjq75U4uslyuy4kn0qNZaxM.woff2",
     "revision": "d15ba972040b09041a17f6a751282dcb"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-dd78478537850a7d9a75.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e00da1ea3e7fe3ae77e6.js"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "6e5356e6680c299d82846862c46218a5"
+    "revision": "61f402d46c4b4e394b13da24635065fe"
   },
   {
     "url": "styles.cdbefcf032504d410e50.css"
@@ -67,7 +67,7 @@ self.__precacheManifest = [
     "url": "b6b1b999d46b12a733ebc699858eab91ef2e633a-0d803d7cf59b6abea6cb.js"
   },
   {
-    "url": "component---src-pages-404-js-451d443d0d31fe308f8b.js"
+    "url": "component---src-pages-404-js-a70d3319481d40aa4551.js"
   },
   {
     "url": "page-data/404/page-data.json",
@@ -78,83 +78,80 @@ self.__precacheManifest = [
     "revision": "d2d3612af1df181302e193fa8c922dd4"
   },
   {
-    "url": "component---src-pages-about-js-db83bf71d45292c4e628.js"
+    "url": "component---src-pages-about-js-2b43b67d32c831b5b864.js"
   },
   {
     "url": "page-data/about/page-data.json",
     "revision": "3ddd5d51b095997b4d473ef99898912f"
   },
   {
-    "url": "component---src-pages-academy-js-9368d402ea3d6e85d069.js"
+    "url": "component---src-pages-academy-js-eaa1e6c39e0b0d1402ce.js"
   },
   {
     "url": "page-data/academy/page-data.json",
     "revision": "802b9b1abc5174a4ec4aaa21c09bb364"
   },
   {
-    "url": "21670ea360c8ace1a999b6cbda7828b6d834e63a-70d3b24a7e60029453f1.js"
+    "url": "f311af33234ddd28e9d0bb1b79eb4fb4693f6c2e-219cf9d5a66c9729d69d.js"
   },
   {
-    "url": "component---src-pages-academy-schedule-js-21663b2ccd81f50a9404.js"
+    "url": "component---src-pages-academy-schedule-js-4341f573c4c667dcf475.js"
   },
   {
     "url": "page-data/academy-schedule/page-data.json",
     "revision": "d06096e7e686dba29a2955848de8200d"
   },
   {
-    "url": "component---src-pages-academygrad-js-a61b45d9d849fe29fbbd.js"
+    "url": "component---src-pages-academygrad-js-042efb18d5fd567ef53e.js"
   },
   {
     "url": "page-data/academygrad/page-data.json",
     "revision": "da10be4f1e14c69912c783c38c22539c"
   },
   {
-    "url": "component---src-pages-courses-js-aa627e17ec73fb1a10a8.js"
+    "url": "component---src-pages-courses-js-3ccc61ea17b18cfb9ed9.js"
   },
   {
     "url": "page-data/courses/page-data.json",
     "revision": "4c55b0a37587ffa7dc418322d0e7972d"
   },
   {
-    "url": "937911d241d7f3ea2546b91f06eb775f38ce2a9b-4bbc43f995f07995a8de.js"
-  },
-  {
-    "url": "component---src-pages-index-js-8f0a7f8b379418cfffe6.js"
+    "url": "component---src-pages-index-js-e31065d819a2b0d41155.js"
   },
   {
     "url": "page-data/index/page-data.json",
     "revision": "1fb79e6fc14d98f88dd9e5819fe647f5"
   },
   {
-    "url": "component---src-pages-instructions-js-3b63ff6426f4f9ad23fa.js"
+    "url": "component---src-pages-instructions-js-cb484cf4997ac66f7dd7.js"
   },
   {
     "url": "page-data/instructions/page-data.json",
     "revision": "98639e9a908cbbbcfc140ae5b57c32a5"
   },
   {
-    "url": "component---src-pages-livestream-js-7625939a593e919fcae2.js"
+    "url": "component---src-pages-livestream-js-5e29112268fa42956de4.js"
   },
   {
     "url": "page-data/livestream/page-data.json",
     "revision": "01027a9290b2df11ac2b79ce73d7a9d1"
   },
   {
-    "url": "component---src-pages-newgrad-js-5c68c15e14cbb53b1010.js"
+    "url": "component---src-pages-newgrad-js-866f05f55a6f45b1c0af.js"
   },
   {
     "url": "page-data/newgrad/page-data.json",
     "revision": "2f5320de9811980d894fee1733ca8d72"
   },
   {
-    "url": "component---src-pages-register-js-040a266c92fdc9de18eb.js"
+    "url": "component---src-pages-register-js-d52b43db4efbae0c1320.js"
   },
   {
     "url": "page-data/register/page-data.json",
     "revision": "5268465e7a2db1b91e8ccc4d43d47fe2"
   },
   {
-    "url": "component---src-pages-schedule-js-7794328b6f41726e2643.js"
+    "url": "component---src-pages-schedule-js-f78a72c0eb1ed56ff153.js"
   },
   {
     "url": "page-data/schedule/page-data.json",
@@ -255,7 +252,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-2e801e2360f8b32ac8b0.js`))) {
+  if (!resources || !(await caches.match(`/app-fe28ff561e652c8a47c5.js`))) {
     return await fetch(event.request)
   }
 
